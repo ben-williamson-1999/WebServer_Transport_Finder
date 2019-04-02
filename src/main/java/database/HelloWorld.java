513ba4709fa546d6322d4468f3d8c5c087c5fbb7
ben-williamson-1999 <ben.williamson1@ntlworld.com>
2019-04-03 00:36:12 +0100

    Inital commit ;)

---
+++ src/main/java/database/HelloWorld.java
@@ -0,0 +1,8 @@
+package database;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Sup world, this is here because i want the file structure");
+    }
+}
